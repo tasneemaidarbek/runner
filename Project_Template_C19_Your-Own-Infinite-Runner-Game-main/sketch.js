2f7dd71ebd0cd05e6fff145e87665c7afd328591
tasneemaidarbek <130894271+tasneemaidarbek@users.noreply.github.com>
2023-09-10 09:41:53 +0400

    Add files via upload

---
+++ Project_Template_C19_Your-Own-Infinite-Runner-Game-main/sketch.js
@@ -0,0 +1,53 @@
+var path,boy,left_boundary,right_boundary;
+var pathImg, boyImg;
+var i;
+
+function preload(){
+    pathImg=loadImage("path.js");
+    boyImg=loadImage("boy.running.js");
+}
+
+function setup(){
+  
+    createCanvas(400,400);
+    
+  
+  path=createSprite(200,200);
+  path.addImage(pathImg);
+  path.velocityY = 4;
+  path.scale=1.2;
+  
+  
+  boy = createSprite(180,340,30,30);
+  boy.scale=0.08;
+  boy.addAnimation("JakeRunning",boyImg);
+    
+  
+  leftBoundary=createSprite(0,0,100,800);
+  
+
+   leftBoundary.invisible = true;
+   leftBoundary.visible = false;
+  
+  
+  rightBoundary=createSprite(410,0,100,800);
+  rightBoundary.visible = false;
+  }
+  
+  function draw() {
+    background(0);
+    path.velocityY = 4;
+    
+    boy.x = World.mouseX;
+    
+    edges= createEdgeSprites();
+    boy.collide(edges[3]);
+    boy.collide(leftBoundary);
+    boy.collide(rightBoundary);
+    
+  
+    if(path.y > 400 ){
+      path.y = height/2;
+    }
+    
+    drawSprites();}
